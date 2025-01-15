4462ba0c008727ae451ae15dc0db88b074f2357d
Jaden Lewis <jlewis@clarku.edu>
2025-01-15 11:18:16 -0500

    Minor updates

---
+++ finance/server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const db = require('./database'); // Import database connection
+
+const app = express();
+const PORT = 8080;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/stocks', (req, res) => {
+    db.all('SELECT * FROM stocks', [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        } else {
+            res.json(rows);
+        }
+    })
+})
